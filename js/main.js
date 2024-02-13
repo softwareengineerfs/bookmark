ec5e7d6863b7fde6711c4a2fea028fb3106e776f
softwareengineerfs <softwareengineerfs@gmail.com>
2024-02-13 17:27:54 +0500

    add faq section and accordion

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
+// TAB PANELS
 const elsTabsItem = document.querySelectorAll(".tabs__item");
 const elsTabsPanel = document.querySelectorAll(".tabs__pannel");
 const elsTabLink = document.querySelectorAll(".js-tab-link");
+
+const elsAccordionItemTogler = document.querySelectorAll(".accordion__item-toggler");
+const elsAccordionItem = document.querySelectorAll(".accordion__item")
+
+function closeAccordionItems () {
+   elsAccordionItem.forEach( function (elAccordionItem) {
+    elAccordionItem.classList.remove("accordion__item--open");
+   });
+};
 
 function deactivateTabItems () {
   elsTabsItem.forEach( function (elTabsItem) {
     elTabsItem.classList.remove("tabs__item--active");
   });
-}
+};
 
 function deactivateTabPanel () {
   elsTabsPanel.forEach( function (elTabsPanel) {
     elTabsPanel.classList.remove("tabs__panel--active");
   });
-}
+};
 
 elsTabLink.forEach( function (elTabLink) {
   elTabLink.addEventListener("click" , function (evt) {
@@ -32,5 +42,14 @@ elsTabLink.forEach( function (elTabLink) {
     const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
     elTargetPanel.classList.add("tabs__panel--active");
 
+  });
+});
+
+
+elsAccordionItemTogler.forEach(function (elAccordionItemToggler) {
+  elAccordionItemToggler.addEventListener("click", function () {
+    closeAccordionItems();
+
+    elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
   });
 });
